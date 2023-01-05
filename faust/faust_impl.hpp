0f79652c29ec9112742a2590306b4ab5a453d53a
Zhou Chang <achang.zhou@gmail.com>
2023-01-05 14:56:00 +0800

    clean code

--- faust/faust_impl.hpp
+++ faust/faust_impl.hpp
@@ -10,7 +10,7 @@ void init_words(Enviroment& env) {
     env.insert_native_word("faust.osc.sine", OscSineWord::creator);
     env.insert_native_word("faust.osc.sawtooth", OscSawtoothWord::creator);
     env.insert_native_word("faust.osc.square", OscSquareWord::creator);
-    env.insert_native_word("faust.no.noise", NoiseWhiteWord::creator);
+    env.insert_native_word("faust.no.white", NoiseWhiteWord::creator);
 }
 
 }}
